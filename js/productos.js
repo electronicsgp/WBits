50e994f2f7f922ec8b75584466f9f368389fc7ba
electronicsgp <pedro.escom.isc@gmail.com>
2021-03-30 12:34:06 -0600

    first commit

---
+++ js/productos.js
@@ -0,0 +1,41 @@
+function choney(e) {
+    e.setAttribute('src', '../assets/images/choneywell.png')
+
+}
+
+function honey(e) {
+    e.setAttribute('src', '../assets/images/honeywell.png')
+
+}
+
+function czebra(e) {
+    e.setAttribute('src', '../assets/images/czebra.png')
+}
+
+function carbor(e) {
+    e.setAttribute('src', '../assets/images/carbor.png')
+}
+
+function zebra(e) {
+    e.setAttribute('src', '../assets/images/zebra.png')
+}
+
+function arbor(e) {
+    e.setAttribute('src', '../assets/images/arbor.png')
+}
+
+function tsc(e) {
+    e.setAttribute('src', '../assets/images/tsc.png')
+}
+
+function seuic(e) {
+    e.setAttribute('src', '../assets/images/seuic.png')
+}
+
+function ctsc(e) {
+    e.setAttribute('src', '../assets/images/ctsc.png')
+}
+
+function cseuic(e) {
+    e.setAttribute('src', '../assets/images/cseuic.png')
+}
