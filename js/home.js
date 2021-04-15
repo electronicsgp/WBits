fadc0c1b699a5c8e5f3d13c21a28a093c22f1e5a
electronicsgp <pedro.escom.isc@gmail.com>
2021-04-15 02:56:59 -0500

    first commit

--- js/home.js
+++ js/home.js
@@ -1,3 +1,31 @@
+const sections = document.querySelectorAll(".sc-section");
+var elemento = document.getElementById("otro");
+window.onscroll = () => scrollspy();
+window.onload = () => scrollspy();
+window.onresize = () => scrollspy();
+
+const scrollspy = () => {
+
+    const pageYPosition =
+        document.documentElement.scrollTop || document.body.scrollTop;
+    sections.forEach((section) => {
+        const sectionYPosition = section.offsetTop;
+        let ejemplo = document.querySelector('#' + section.id);
+        if (pageYPosition > sectionYPosition - 700) {
+            // alert(section.id);
+            if ("test" === section.id) {
+                // alert("hola");
+                elemento.className += " animate__animated animate__fadeInUp";
+
+            }
+        } else {
+            elemento.className = "row  d-flex justify-content-center mx-xl-5";
+        }
+    });
+};
+
+scrollspy();
+
 function axtec(e) {
     e.setAttribute('src', './assets/images/axtec.png')
 
