663e9d90bd49a121427da709eaaa773a2bf6bc74
electronicsgp <pedro.escom.isc@gmail.com>
2021-04-15 14:14:18 -0500

    first commit

--- js/productos.js
+++ js/productos.js
@@ -1,51 +1,139 @@
-function choney(e) {
-    e.setAttribute('src', '../assets/images/choneywell.png')
+function cuno(e) {
+    e.setAttribute('src', '../assets/images/productos/color/1a.svg')
 
 }
 
-function honey(e) {
-    e.setAttribute('src', '../assets/images/honeywell.png')
+function uno(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/1.svg')
 
 }
 
-function czebra(e) {
-    e.setAttribute('src', '../assets/images/czebra.png')
-}
-
-function carbor(e) {
-    e.setAttribute('src', '../assets/images/carbor.png')
-}
-
-function zebra(e) {
-    e.setAttribute('src', '../assets/images/zebra.png')
-}
-
-function arbor(e) {
-    e.setAttribute('src', '../assets/images/arbor.png')
-}
-
-function tsc(e) {
-    e.setAttribute('src', '../assets/images/tsc.png')
-}
-
-function seuic(e) {
-    e.setAttribute('src', '../assets/images/seuic.png')
-}
-
-function ctsc(e) {
-    e.setAttribute('src', '../assets/images/ctsc.png')
-}
-
-function cseuic(e) {
-    e.setAttribute('src', '../assets/images/cseuic.png')
-}
-
-function axtec(e) {
-    e.setAttribute('src', '../assets/images/axtec.png')
+function cdos(e) {
+    e.setAttribute('src', '../assets/images/productos/color/2a.svg')
 
 }
 
-function caxtec(e) {
-    e.setAttribute('src', '../assets/images/caxtec.png')
+function dos(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/2.svg')
+
+}
+
+function ctres(e) {
+    e.setAttribute('src', '../assets/images/productos/color/3a.svg')
+
+}
+
+function tres(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/3.svg')
+
+}
+
+function ccuatro(e) {
+    e.setAttribute('src', '../assets/images/productos/color/4a.svg')
+
+}
+
+function cuatro(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/4.svg')
+
+}
+
+function ccinco(e) {
+    e.setAttribute('src', '../assets/images/productos/color/5a.svg')
+
+}
+
+function cinco(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/5.svg')
+
+}
+
+function cseis(e) {
+    e.setAttribute('src', '../assets/images/productos/color/6a.svg')
+
+}
+
+function seis(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/6.svg')
+
+}
+
+function csiete(e) {
+    e.setAttribute('src', '../assets/images/productos/color/7a.svg')
+
+}
+
+function siete(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/7.svg')
+
+}
+
+function cocho(e) {
+    e.setAttribute('src', '../assets/images/productos/color/8a.svg')
+
+}
+
+function ocho(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/8.svg')
+
+}
+
+function cnueve(e) {
+    e.setAttribute('src', '../assets/images/productos/color/9a.svg')
+
+}
+
+function nueve(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/9.svg')
+
+}
+
+function cdiez(e) {
+    e.setAttribute('src', '../assets/images/productos/color/10a.svg')
+
+}
+
+function diez(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/10.svg')
+
+}
+
+function conce(e) {
+    e.setAttribute('src', '../assets/images/productos/color/11a.svg')
+
+}
+
+function once(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/11.svg')
+
+}
+
+function cdoce(e) {
+    e.setAttribute('src', '../assets/images/productos/color/12a.svg')
+
+}
+
+function doce(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/12.svg')
+
+}
+
+function ctrece(e) {
+    e.setAttribute('src', '../assets/images/productos/color/13a.svg')
+
+}
+
+function trece(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/13.svg')
+
+}
+
+function ccator(e) {
+    e.setAttribute('src', '../assets/images/productos/color/14a.svg')
+
+}
+
+function cator(e) {
+    e.setAttribute('src', '../assets/images/productos/Negros/14.svg')
 
 }
